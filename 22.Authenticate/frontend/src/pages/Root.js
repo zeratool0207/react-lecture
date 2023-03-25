f40bbe337f54e7b1e42bb6386b835ee87a09c2ff
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-03-26 07:39:15 +0900

    0326 commit & push

--- 22.Authenticate/frontend/src/pages/Root.js
+++ 22.Authenticate/frontend/src/pages/Root.js
@@ -1,9 +1,36 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { 
+  Outlet, 
+  useLoaderData, 
+  useSubmit 
+} from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
+  const submit = useSubmit();
+  const token = useLoaderData();
   // const navigation = useNavigation();
+
+  useEffect( () => {
+    if ( !token ) {
+      return ;
+    }
+
+    if ( token === 'EXPIRED' ) {
+      submit(null, {action: '/logout', method: 'post'});
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+
+    setTimeout(() => {
+      submit(null, {action: '/logout', method: 'post'})
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
