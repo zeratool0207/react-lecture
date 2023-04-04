f688b7e466523ae8de111b6d53ff65927e0a6a72
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-04-05 07:36:02 +0900

    routing modify...

--- 20.react-router/src/App.js
+++ 20.react-router/src/App.js
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductPage from './pages/Products';
 import RootLayout from './pages/Root';
+import Error from './pages/Error';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element:<RootLayout />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductPage /> },
